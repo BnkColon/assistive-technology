df67d30bd3e46c83d1bd5082d192d9879307e760
Bianca Colon <colon.bianca@gmail.com>
2016-11-03 19:41:28 -0400

    Try Catch added, this should eliminate the infinite loop

--- test/tryingToCatchClick.py
+++ test/tryingToCatchClick.py
@@ -8,9 +8,11 @@ width, height = pyautogui.size()
 pyautogui.moveTo(5,5)
 half = height/2
 
-for x in range(0, height):
-	pyautogui.moveRel(height, 0, duration=0.45)
-	if pyautogui.click() == True:
-		pyautogui.moveRel(0, width, duration=0.45)
-	
+try:
+	for x in range(0, height):
+		pyautogui.moveRel(height, 0, duration=0.45)
+		if pyautogui.click() == True:
+			pyautogui.moveRel(0, width, duration=0.45)
+except KeyboardInterrupt:
+    print('\n')	
 
